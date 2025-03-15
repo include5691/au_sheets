ec01f05590372d356653536c5fe708bdd6c62cc7
Andrey Pshenitsyn <include5691@gmail.com>
2025-03-15 10:50:58 +0500

    style: remove unused import && replace 'space colon' to just 'colon'

--- au_sheets/get.py
+++ au_sheets/get.py
@@ -3,7 +3,7 @@ from pandas import DataFrame
 from gspread.exceptions import GSpreadException
 from ._worksheet import get_worksheet
 
-def get_df(table_name : str, sheet_name: str, create_sheet: bool = False) -> DataFrame | None:
+def get_df(table_name: str, sheet_name: str, create_sheet: bool = False) -> DataFrame | None:
     """Get sheet data as a DataFrame"""
     worksheet = get_worksheet(table_name, sheet_name, create_sheet)
     if not worksheet:
--- au_sheets/update.py
+++ au_sheets/update.py
@@ -1,5 +1,4 @@
 import logging
-import numpy as np
 from pandas import DataFrame
 from gspread.exceptions import APIError
 from ._worksheet import get_worksheet
@@ -10,7 +9,7 @@ def _get_columns_addr(n: int) -> str:
         return chr(65 + n)
     return _get_columns_addr(n // 26 - 1) + chr(65 + n % 26)
 
-def update_sheet(df: DataFrame, table_name : str, sheet_name: str, create_sheet: bool = False) -> None:
+def update_sheet(df: DataFrame, table_name: str, sheet_name: str, create_sheet: bool = False) -> None:
     """Update Google Sheet with DataFrame values"""
     if not isinstance(df, DataFrame):
         logging.error("Invalid DataFrame")
